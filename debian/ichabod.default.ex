475800bfd9593d52e4e3679f036773d1ef8c0922
Jason Knight <jason@jason-knight-martin.com>
2013-04-14 07:53:18 -0400

    debianifying

---
+++ debian/ichabod.default.ex
@@ -0,0 +1,10 @@
+# Defaults for ichabod initscript
+# sourced by /etc/init.d/ichabod
+# installed at /etc/default/ichabod by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
