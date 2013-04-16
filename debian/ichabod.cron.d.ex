36e1a25420c42f063cf098e77b6a675fb35a3fc4
Michael Franzl <office@michaelfranzl.com>
2013-04-16 14:12:18 +0200

    removed debian dir

--- debian/ichabod.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the ichabod package
-#
-0 4	* * *	root	[ -x /usr/bin/ichabod_maintenance ] && /usr/bin/ichabod_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(ichabod):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="ichabod" command="/usr/bin/ichabod"
